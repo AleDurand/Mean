c9fb4f51948a25dbac389fa206e8bcf677534f84
Alejandro Durand <alejandro.durand.90@gmail.com>
2016-01-18 23:57:14 -0300

    Fixed angular routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,21 @@
-angular.module('meanApp', ['ngRoute']);
+angular.module('meanApp', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/',{
+			redirectTo: '/albums'
+		})
+		.when('/albums',{
+			templateUrl: 'templates/pages/albums/index.html',
+			controller: 'AlbumsIndexController',
+			controllerAs: 'indexController'
+		})
+		.when('/albums/:id', {
+			templateUrl: 'templates/pages/albums/show.html',
+			controller: 'AlbumsShowController',
+			controllerAs: 'showController'
+		})
+		.otherwise({ redirectTo: '/' });
+}]);
 
 
 

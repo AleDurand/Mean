648f94ad502983e0f4526b12c097040f93306f64
Alejandro Durand <alejandro.durand.90@gmail.com>
2016-02-03 00:51:08 -0300

    First implementation to send an email. (working). Replace 'valid_email' with a valid email.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,13 +1,23 @@
 'use strict';
 
 var errorHandler = require('../errors/errorHandler');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport('smtps://contact.designer.bb%40gmail.com:Password1%40@smtp.gmail.com');
+
+var mailOptions = {
+    from: 'Fred Foo 👥 <contact.designer.bb@gmail.com>', 
+    to: 'valid_email@gmail.com',
+    subject: 'Hello ✔', 
+    text: 'Hello world 🐴',
+    html: '<b>Hello world 🐴</b>' 
+};
 
 exports.sendEmail = function (req, res) {
-    try {
-        return res.status(204).end();
-    }
-    catch (error) {
-        return res.status(400).send({ message: errorHandler.getErrorMessage(error) });
-    }
-
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return res.status(400).send({ message: errorHandler.getErrorMessage(error) });
+        }
+        return res.status(204).end()
+    });
 };

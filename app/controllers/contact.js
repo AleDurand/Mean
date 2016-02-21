896008ed0c75b48e6af91ef0ea3862cb622694f2
Alejandro Durand <alejandro.durand.90@gmail.com>
2016-02-21 20:08:00 -0300

    Updated message template

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -3,19 +3,19 @@
 var errorHandler = require('../errors/errorHandler');
 var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport('smtps://contact.designer.bb%40gmail.com:Password1%40@smtp.googlemail.com');
-var send = transporter.templateSender(template);
-
 var template = {
     from: 'Administrator <contact.designer.bb@gmail.com>',
     to: 'alejandro.durand.90@gmail.com',
     subject: 'Contact {{address}}',
-    text: '{{message}} \n\n {{name}}\n{{phone}}',
-    html: '{{message}} \n\n {{name}}\n{{phone}}'
+    text: '{{message}} {{name}} {{phone}}',
+    html: '{{message}} <br><br> <b>{{name}}<br>{{phone}}</b>'
 };
 
+var transporter = nodemailer.createTransport('smtps://contact.designer.bb%40gmail.com:Password1%40@smtp.googlemail.com');
+var send = transporter.templateSender(template);
+
 exports.sendEmail = function (req, res) {
-    send(template, req.body, function (error, info) {
+    send({}, req.body, function (error, info) {
         if (error) {
             return res.status(400).send({ message: errorHandler.getErrorMessage(error) });
         }

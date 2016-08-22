bdb03a91cf8596407b3d20101081a1edd523b6c5
Mariano <mrnrodecker@gmail.com>
2016-08-22 19:32:16 -0300

    Name of folder changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express  = require('express');
 var app      = express();                               // create our app w/ express
 var mongoose = require('mongoose');                     // mongoose for mongodb
-var port     = process.env.PORT || 50000;                // set the port
+var port     = 50000;                // set the port
 var database = require('./config/database');            // load the database config
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
@@ -14,7 +14,7 @@ var media = require('./config/media');
 // configuration ===============================================================
 mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/private'));                 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/media'));   
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.json({limit: '50mb'}));

820a73718691d60ff1c70fce6757e0c10e97e11b
AleDurand <alejandro.durand.90@gmail.com>
2017-04-14 06:09:13 -0300

    Updated dependencies. Added ng2-materialize dependency. Fixed footer.

--- client2/src/app/app.component.ts
+++ client2/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, Optional } from '@angular/core';
 
+export interface Tab {
+  title: string;
+  component: any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +12,25 @@ import { Component, Optional } from '@angular/core';
 })
 export class AppComponent {
 
+  public tabs: Array<Tab>;
+  public selectedTab: number = 0;
+  public smallResolution: boolean;
+
   constructor() {
+    this.tabs = [
+      { title: 'Inicio', component: '' },
+      { title: '15 a\u00F1os', component: '' },
+      { title: 'Bautismos', component: '' },
+      { title: 'Otros eventos', component: '' },
+      { title: 'Contacto', component: '' }
+    ];
+  }
+
+  updateSelectedTab($event) {
+    this.selectedTab = $event;
+  }
+
+  showMenu() {
 
   }
 
--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-
-import 'hammerjs';
+import { MaterializeModule } from 'ng2-materialize';
 
 import { AppComponent } from './app.component';
 
@@ -14,10 +11,10 @@ import { PipesModule } from '../pipes/pipes.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule, BrowserAnimationsModule,
+    BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot(),
+    MaterializeModule.forRoot(),
     PipesModule
   ],
   bootstrap: [AppComponent],

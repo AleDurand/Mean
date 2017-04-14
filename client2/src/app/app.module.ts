df98b743ee60eee81daaf83dbe47c14573a1eaa7
AleDurand <alejandro.durand.90@gmail.com>
2017-04-14 02:13:12 -0300

    Update HomePage. First attempt to integrate sass into the project.

--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -2,22 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
 import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule.forRoot(),
     PipesModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [],
+  providers: []
 })
 export class AppModule { }

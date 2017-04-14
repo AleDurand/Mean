117845f3c69d012256772d3fdb5788f4fa8685b6
AleDurand <alejandro.durand.90@gmail.com>
2017-04-14 00:52:21 -0300

    First commit angular-4

---
+++ client2/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+import { PipesModule } from '../pipes/pipes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    PipesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client2/src/services/album.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+import { } from './safe-http.service'
+
+@Injectable()
+export class AlbumService {
+
+  constructor() {
+
+  }
+
+}

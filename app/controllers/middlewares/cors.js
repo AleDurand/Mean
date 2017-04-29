b5405b3c8bb59b6c6b7acb3ded95c315101dd0e7
AleDurand <alejandro.durand.90@gmail.com>
2017-04-29 04:10:16 -0300

    Added new header

--- app/controllers/middlewares/cors.js
+++ app/controllers/middlewares/cors.js
@@ -3,7 +3,7 @@
 exports.cors = function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Frame-Options, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
   next();
 };
